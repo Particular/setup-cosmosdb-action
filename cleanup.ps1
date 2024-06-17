72ff19b4b89fd6c513882c85ab6029928b9bd84f
Daniel Marbach <daniel.marbach@openplace.net>
2024-06-17 21:21:14 +0200

    Proper URI

--- cleanup.ps1
+++ cleanup.ps1
@@ -7,4 +7,7 @@ param (
 #$ignore = az cosmosdb delete --resource-group GitHubActions-RG --name $cosmosName --yes
 
 # az rest method
-az rest --method DELETE --uri "https://management.azure.com/subscriptions/$($credentials.subscriptionId)/resourceGroups/GitHubActions-RG/providers/Microsoft.DocumentDB/databaseAccounts/$($cosmosName)?api-version=2021-04-15"
+$credentials = $azureCredentials | ConvertFrom-Json
+$subscriptionId = $credentials.subscriptionId
+az rest --method DELETE `
+     --uri https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/GitHubActions-RG/providers/Microsoft.DocumentDB/databaseAccounts/$cosmosName?api-version=2021-04-15"
