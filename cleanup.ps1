909eb667912d2cb7827b9d5bd572672158cdeba2
Daniel Marbach <daniel.marbach@openplace.net>
2024-06-18 08:06:07 +0200

    Use AZ Rest to avoid the custom token retrieval (#31)
    * Try using az rest
    
    * One liner
    
    * Proper URI
    
    * Update cleanup.ps1
    
    * Escaping

--- cleanup.ps1
+++ cleanup.ps1
@@ -6,9 +6,8 @@ param (
 # Use this once cosmosdb delete offers --no-wait argument, until then, it takes too long (~7m) and we use curl instead
 #$ignore = az cosmosdb delete --resource-group GitHubActions-RG --name $cosmosName --yes
 
-# curl-based method
+# az rest method
 $credentials = $azureCredentials | ConvertFrom-Json
-$token = curl -X POST -d "grant_type=client_credentials&client_id=$($credentials.clientId)&client_secret=$($credentials.clientSecret)&resource=https%3A%2F%2Fmanagement.azure.com%2F" https://login.microsoftonline.com/$($credentials.tenantId)/oauth2/token | ConvertFrom-Json
-$authHeader = "Authorization: Bearer $($token.access_token)"
-$resourceUrl = "https://management.azure.com/subscriptions/$($credentials.subscriptionId)/resourceGroups/GitHubActions-RG/providers/Microsoft.DocumentDB/databaseAccounts/$($cosmosName)?api-version=2021-04-15"
-curl -X DELETE $resourceUrl -H $authHeader -H "Content-Type: application/json" --silent
+$subscriptionId = $credentials.subscriptionId
+az rest --method DELETE `
+     --uri https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/GitHubActions-RG/providers/Microsoft.DocumentDB/databaseAccounts/${cosmosName}?api-version=2021-04-15
