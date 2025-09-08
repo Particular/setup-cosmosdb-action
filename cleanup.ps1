851c230f8d7a5c44ce7171036a4541529769e9d2
Daniel Marbach <daniel.marbach@openplace.net>
2025-09-08 21:57:15 +0200

    Refactor cosmosdb delete command to use no-wait

--- cleanup.ps1
+++ cleanup.ps1
@@ -5,11 +5,4 @@ param (
 
 $resourceGroup = $Env:RESOURCE_GROUP_OVERRIDE ?? "GitHubActions-RG"
 
-# Use this once cosmosdb delete offers --no-wait argument, until then, it takes too long (~7m) and we use curl instead
-#$ignore = az cosmosdb delete --resource-group GitHubActions-RG --name $cosmosName --yes
-
-# az rest method
-$credentials = $azureCredentials | ConvertFrom-Json
-$subscriptionId = $credentials.subscriptionId
-az rest --method DELETE `
-     --uri https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$($resourceGroup)/providers/Microsoft.DocumentDB/databaseAccounts/${cosmosName}?api-version=2021-04-15
+$ignore = az cosmosdb delete --resource-group $resourceGroup --name $cosmosName --yes --no-wait
